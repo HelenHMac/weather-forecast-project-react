65ed7d38051dad4eb1406c5fc66d98198f4c773b
HelenHMac <120410707+HelenHMac@users.noreply.github.com>
2023-01-08 22:38:15 -0500

    updated weather css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import React from "react";
 import './App.css';
+import Weather from "./Weather"
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      Weather App
+      <Weather />
       <footer>
       This Project was created by <a href="https://luminous-kelpie-26467d.netlify.app" target="_blank" rel="noreferrer"> Olena Honcharova </a> and is {" "}
       <a href="https://github.com/HelenHMac/weather-forecast-project-react" target="_blank" rel="noreferrer">
---
+++ src/Weather.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+return (
+    <div className="Weather">
+          <h1 className="app-header">Weather Forecast App</h1>
+          <form action="" autofocus="on" className="mb-3">
+          <div className="row">
+            <div className="col-8">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                class="form-control"
+                autocomplete="off"
+              />
+            </div>
+            <div className="col-4">
+              <input type="submit" value="Search" className="btn main-button" />
+            </div>
+          </div>
+        </form>
+
+        <div className="row">
+            <div className="col-7">
+             <ul className="listnone">
+                <li className="main-city">New York</li>
+                <li className="main-description">
+                  Wednesday, cloudy
+                </li>
+                <li className="main-description">
+                  Humidity: 75%, Wind: 5km/h
+                </li>
+              </ul>
+              </div>
+              <div className="col-5">
+              <span>
+                <strong className="main-temperature">
+                 5
+                </strong>
+                <span className="main-degree">°C | F </span>
+              </span>
+              <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="" className="main-img" />
+              </div>
+        </div>
+    </div>
+    )
+}
