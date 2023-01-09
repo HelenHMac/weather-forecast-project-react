94fe9a083ca9b156c2a921caea562eb3317c45d7
HelenHMac <120410707+HelenHMac@users.noreply.github.com>
2023-01-08 21:50:54 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       Weather App
       <footer>
       This Project was created by <a href="https://luminous-kelpie-26467d.netlify.app" target="_blank" rel="noreferrer"> Olena Honcharova </a> and is {" "}
@@ -12,6 +13,7 @@ export default function App() {
         open-sourced on GitHub
       </a>
       </footer>
+      </div>
     </div>
   );
 }
